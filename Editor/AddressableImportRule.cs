51a5b9f19f63c9b84b369f20d373e9183ce18663
Favo Yang <favoyang@gmail.com>
2019-08-04 11:05:35 +0800

    Tweaked property label.

--- Editor/AddressableImportRule.cs
+++ Editor/AddressableImportRule.cs
@@ -64,6 +64,7 @@ public class AddressableImportRule
     /// Simplify address.
     /// </summary>
     [Tooltip("Simplify address to filename without extension.")]
+    [Label("Address Simplified")]
     public bool simplified;
 
     /// <summary>
---
+++ Editor/Helper/LabelAttribute.cs
@@ -0,0 +1,47 @@
+/// <summary>
+/// LabelAttribute,
+/// modified from https://github.com/dbrizov/NaughtyAttributes/blob/master/Assets/NaughtyAttributes/Scripts/Editor/PropertyDrawers/LabelPropertyDrawer.cs
+/// </summary>
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+namespace UnityAddressableImporter.Helper
+{
+    [AttributeUsage(AttributeTargets.Field)]
+    public class LabelAttribute : PropertyAttribute
+    {
+        public string Label { get; private set; }
+
+        public LabelAttribute(string label)
+        {
+            this.Label = label;
+        }
+    }
+}
+
+#if UNITY_EDITOR
+namespace UnityAddressableImporter.Helper.Internal
+{
+	[CustomPropertyDrawer(typeof(LabelAttribute))]
+	public class LabelAttributeDrawer : PropertyDrawer
+	{
+		private LabelAttribute Attribute
+		{
+			get { return _attribute ?? (_attribute = attribute as LabelAttribute); }
+		}
+
+		private LabelAttribute _attribute;
+
+		public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+		{
+            var guiContent = new GUIContent(Attribute.Label);
+			EditorGUI.PropertyField(position, property, guiContent, true);
+		}
+	}
+}
+#endif
