101ef285707fc01e5a075589c63b8b77d66206f8
Mefodei <mefodei.link@gmail.com>
2020-06-04 15:30:04 +0300

    - add optional Odin inspector support - move Button Attribute to runtime asm.def

---
+++ Editor/Helper/AddressablesImporterOdinInspectorDrawer.cs
@@ -0,0 +1,36 @@
+﻿namespace UnityAddressableImporter.Editor.Helper
+{
+    using System;
+    using System.Diagnostics;
+    using Object = UnityEngine.Object;
+
+    public class AddressablesImporterOdinInspectorDrawer : IDisposable
+    {
+        private Object _target;
+#if ODIN_INSPECTOR
+        private Sirenix.OdinInspector.Editor.PropertyTree _drawer;
+#endif
+
+        [Conditional("ODIN_INSPECTOR")]
+        public void Initialize(Object target)
+        {
+            _target = target;
+            _drawer = Sirenix.OdinInspector.Editor.PropertyTree.Create(_target);
+        } 
+        
+        [Conditional("ODIN_INSPECTOR")]
+        public void Draw()
+        {
+            if (!_target) return;
+            _drawer.Draw(false);
+        }
+
+        public void Dispose()
+        {
+            _target = null;
+#if ODIN_INSPECTOR
+            _drawer?.Dispose();
+#endif
+        }
+    }
+}
