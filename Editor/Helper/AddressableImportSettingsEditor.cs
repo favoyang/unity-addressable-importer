b8cb290c4ab984e8692210b1ab0a303b3bb0d0ff
Mefodei <mefodei.link@gmail.com>
2021-05-16 17:08:28 +0300

    - odin 3 support changes - remove old odin editor handlers - add additional filter validators

--- Editor/Helper/AddressableImportSettingsEditor.cs
+++ Editor/Helper/AddressableImportSettingsEditor.cs
@@ -2,62 +2,84 @@
 /// ButtonMethodAttribute,
 /// modified from https://github.com/Deadcows/MyBox/blob/master/Attributes/ButtonMethodAttribute.cs
 /// </summary>
-using UnityEngine;
-
 namespace UnityAddressableImporter.Helper.Internal
 {
-    using System;
     using System.Collections.Generic;
     using System.Reflection;
-    using Editor.Helper;
     using UnityEditor;
 
+#if ODIN_INSPECTOR
+    using Sirenix.OdinInspector;
+    using Sirenix.OdinInspector.Editor;
+#endif
 
     [CustomEditor(typeof(AddressableImportSettings), true), CanEditMultipleObjects]
     public class AddressableImportSettingsEditor : Editor
     {
         private List<MethodInfo> _methods;
         private AddressableImportSettings _target;
-        private AddressableImporterOdinHandler _drawer;
 
-
+#if ODIN_INSPECTOR
+        private PropertyTree _propertyTree;
+#endif
+        
         private void OnEnable()
         {
             _target = target as AddressableImportSettings;
-            _drawer = _drawer ?? new AddressableImporterOdinHandler();
             if (_target == null) return;
 
-            _drawer.Initialize(_target);
+#if ODIN_INSPECTOR
+            _propertyTree = PropertyTree.Create(_target);
+#endif
+            
             _methods = AddressableImporterMethodHandler.CollectValidMembers(_target.GetType());
-
-        }
-
-        private void OnDisable()
-        {
-            _drawer.Dispose();
         }
 
         public override void OnInspectorGUI()
         {
-            DrawBaseEditor();
+            
+            if (DrawWithOdin() == false)
+            {
+                DrawBaseEditor();
+            }
 
-#if !ODIN_INSPECTOR
-            if (_methods == null) return;
-
-            AddressableImporterMethodHandler.OnInspectorGUI(_target, _methods);
-#endif
-
+            DrawCommands();
+            
             serializedObject.ApplyModifiedProperties();
-
         }
 
         private void DrawBaseEditor()
         {
-#if ODIN_INSPECTOR
-			_drawer.Draw();
-#else
             base.OnInspectorGUI();
+        }
+
+        private void DrawCommands()
+        {
+            if (_methods == null) return;
+            AddressableImporterMethodHandler.OnInspectorGUI(_target, _methods);
+        }
+        
+        private void OnDisable()
+        {
+#if ODIN_INSPECTOR
+            _propertyTree?.Dispose();
 #endif
+        }
+
+        private bool DrawWithOdin()
+        {
+            var hasOdinAsset = false;
+#if ODIN_INSPECTOR_3
+            hasOdinAsset = true;
+#endif
+            if (!hasOdinAsset) return false;
+
+#if ODIN_INSPECTOR
+            _propertyTree?.Draw();
+            _propertyTree?.ApplyChanges();
+#endif
+            
+            return true;
         }
     }
 }
---
+++ Editor/ISearchFilterable.cs
@@ -0,0 +1,8 @@
+#if !ODIN_INSPECTOR_3
+
+    public interface ISearchFilterable
+    {
+        bool IsMatch(string searchString);
+    }
+    
+#endif
