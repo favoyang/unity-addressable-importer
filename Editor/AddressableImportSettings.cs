8004b5ccb8299307001bb8cbe14ce2925d84167d
Favo Yang <favoyang@gmail.com>
2022-02-06 23:37:20 +0800

    fix: allow load import settings file independent to the default location (#64, #68)
    Co-authored-by: BuildMachine.Vince <vincewang10a7@gmail.com>

--- Editor/AddressableImportSettings.cs
+++ Editor/AddressableImportSettings.cs
@@ -9,10 +9,10 @@ using UnityAddressableImporter.Helper;
 using Sirenix.OdinInspector;
 #endif
 
-[CreateAssetMenu(fileName = "AddressableImportSettings", menuName = "Addressable Assets/Import Settings", order = 50)]
+[CreateAssetMenu(fileName = "AddressableImportSettings", menuName = "Addressables/Import Settings", order = 50)]
 public class AddressableImportSettings : ScriptableObject
 {
-    public const string kDefaultConfigObjectName = "addressableimportsettings";
+    public const string kConfigObjectName = "addressableimportsettings";
     public const string kDefaultPath = "Assets/AddressableAssetsData/AddressableImportSettings.asset";
 
     [Tooltip("Creates a group if the specified group doesn't exist.")]
@@ -63,14 +63,25 @@ public class AddressableImportSettings : ScriptableObject
         get
         {
             AddressableImportSettings so;
-            // Try to locate settings via EditorBuildSettings.
-            if (EditorBuildSettings.TryGetConfigObject(kDefaultConfigObjectName, out so))
+            // Try to locate settings from EditorBuildSettings
+            if (EditorBuildSettings.TryGetConfigObject(kConfigObjectName, out so))
                 return so;
-            // Try to locate settings via path.
+            // Try to locate settings from default path
             so = AssetDatabase.LoadAssetAtPath<AddressableImportSettings>(kDefaultPath);
-            if (so != null)
-                EditorBuildSettings.AddConfigObject(kDefaultConfigObjectName, so, true);
-            return so;
+            if (so != null) {
+                EditorBuildSettings.AddConfigObject(kConfigObjectName, so, true);
+                return so;
+            }
+            // Try to locate settings from AssetDatabase
+            var path = AssetDatabase.FindAssets($"t:{nameof(AddressableImportSettings)}");
+            if (path.Length > 0) {
+                var assetPath = AssetDatabase.GUIDToAssetPath(path[0]);
+                so = AssetDatabase.LoadAssetAtPath<AddressableImportSettings>(assetPath);
+                EditorBuildSettings.AddConfigObject(kConfigObjectName, so, true);
+                return so;
+            }
+            return null;
         }
     }
+
 }
